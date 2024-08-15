967c54bd636f7d2938113698ff34f4cfc202c0f6
shubeeth001 <shubeeth001@warren.k12.in.us>
2024-08-15 13:12:56 -0400

    updt

--- src/speedconverter.java
+++ src/speedconverter.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class speedconverter {
+public class speedconverter {
+    public static void main(String[] args) {
+        System.out.println(toMilesPerHour(1.5));
+        System.out.println(toMilesPerHour(10.25));
+        System.out.println(toMilesPerHour(-5.6));
+        System.out.println(toMilesPerHour(25.42));
+        System.out.println(toMilesPerHour(75.114));
+    }
+
+    public static long toMilesPerHour(double kph) {
+        if (kph < 0) {
+            return -1;
+        } else {
+            double mph = kph / 1.609;
+            return Math.round(mph);
+        }
+    }
 }
